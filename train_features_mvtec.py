ba90a49710b3a33e61c4df294c0614287fe0d0c2
Minoo <24471953+FatemeBameri@users.noreply.github.com>
2025-10-30 00:48:46 +0330

    Create train_features_mvtec.py

---
+++ train_features_mvtec.py
@@ -0,0 +1,368 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+from PIL import Image
+import numpy as np
+from tqdm import tqdm
+import copy
+
+import torch
+from torchvision.models import wide_resnet50_2
+import common  # مطمئن شو این ماژول در مسیرت وجود دارد
+
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+
+class _BaseMerger:
+    def __init__(self):
+        """Merges feature embedding by name."""
+
+    def merge(self, features: list):
+        features = [self._reduce(feature) for feature in features]
+        return np.concatenate(features, axis=1)
+
+
+class PatchCore(torch.nn.Module):
+    def __init__(self, device):
+        """PatchCore anomaly detection class."""
+        super(PatchCore, self).__init__()
+        self.device = device
+
+    def load(
+        self,
+        backbone,
+        layers_to_extract_from,
+        device,
+        input_shape,
+        pretrain_embed_dimension,
+        target_embed_dimension
+
+    ):
+        self.backbone = backbone.to(device)
+        self.layers_to_extract_from = layers_to_extract_from
+        self.input_shape = input_shape
+
+        self.device = device
+        self.patch_maker = PatchMaker(3, stride=1)
+        self.forward_modules = torch.nn.ModuleDict({})
+
+        feature_aggregator = common.NetworkFeatureAggregator(
+            self.backbone, self.layers_to_extract_from, self.device
+        )
+        feature_dimensions = feature_aggregator.feature_dimensions(input_shape)
+        self.forward_modules["feature_aggregator"] = feature_aggregator
+
+        preprocessing = common.Preprocessing(
+            feature_dimensions, pretrain_embed_dimension
+        )
+        self.forward_modules["preprocessing"] = preprocessing
+
+        self.target_embed_dimension = target_embed_dimension
+        preadapt_aggregator = common.Aggregator(
+            target_dim=target_embed_dimension
+        )
+
+        _ = preadapt_aggregator.to(self.device)
+
+        self.forward_modules["preadapt_aggregator"] = preadapt_aggregator
+
+
+    def embed(self, data):
+        if isinstance(data, torch.utils.data.DataLoader):
+            features = []
+            for image in data:
+                if isinstance(image, dict):
+                    image = image["image"]
+                with torch.no_grad():
+                    input_image = image.to(torch.float).to(self.device)
+                    features.append(self._embed(input_image))
+            return features
+        return self._embed(data)
+
+    def _embed(self, images, detach=True, provide_patch_shapes=False):
+        """Returns feature embeddings for images."""
+
+        def _detach(features):
+            if detach:
+                return [x.detach().cpu().numpy() for x in features]
+            return features
+
+        _ = self.forward_modules["feature_aggregator"].eval()
+        with torch.no_grad():
+            features = self.forward_modules["feature_aggregator"](images)
+
+        features = [features[layer] for layer in self.layers_to_extract_from]
+
+
+        #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        '''
+        features = [
+            self.patch_maker.patchify(x, return_spatial_info=True) for x in features
+        ]
+        patch_shapes = [x[1] for x in features]
+        features = [x[0] for x in features]
+        ref_num_patches = patch_shapes[0]
+
+        for i in range(1, len(features)):
+            _features = features[i]
+            patch_dims = patch_shapes[i]
+
+            # TODO(pgehler): Add comments
+            _features = _features.reshape(
+                _features.shape[0], patch_dims[0], patch_dims[1], *_features.shape[2:]
+            )
+            _features = _features.permute(0, -3, -2, -1, 1, 2)
+            perm_base_shape = _features.shape
+            _features = _features.reshape(-1, *_features.shape[-2:])
+            _features = F.interpolate(
+                _features.unsqueeze(1),
+                size=(ref_num_patches[0], ref_num_patches[1]),
+                mode="bilinear",
+                align_corners=False,
+            )
+            _features = _features.squeeze(1)
+            _features = _features.reshape(
+                *perm_base_shape[:-2], ref_num_patches[0], ref_num_patches[1]
+            )
+            _features = _features.permute(0, -2, -1, 1, 2, 3)
+            _features = _features.reshape(len(_features), -1, *_features.shape[-3:])
+            features[i] = _features
+        features = [x.reshape(-1, *x.shape[-3:]) for x in features]
+
+'''
+
+        # As different feature backbones & patching provide differently
+        # sized features, these are brought into the correct form here.
+        #features = self.forward_modules["preprocessing"](features)
+        #features = self.forward_modules["preadapt_aggregator"](features)
+
+        f2 = features[0]  # لایه دوم
+        f3 = features[1]  # لایه سوم
+        f3_up = F.interpolate(f3, size=f2.shape[2:], mode='bilinear', align_corners=False)
+        fused = torch.cat([f2, f3_up], dim=1)
+        features = [fused]
+        features = (features)[0].squeeze(0)
+
+
+        #if provide_patch_shapes:
+            #return _detach(features), patch_shapes
+            #return _detach(features), None
+        #return _detach(features)[0]
+        return (features.detach().cpu().numpy())
+
+
+class AverageMerger(_BaseMerger):
+    @staticmethod
+    def _reduce(features):
+        # NxCxWxH -> NxC
+        return features.reshape([features.shape[0], features.shape[1], -1]).mean(
+            axis=-1
+        )
+
+
+class Aggregator(torch.nn.Module):
+    def __init__(self, target_dim):
+        super(Aggregator, self).__init__()
+        self.target_dim = target_dim
+
+    def forward(self, features):
+        """Returns reshaped and average pooled features."""
+        # batchsize x number_of_layers x input_dim -> batchsize x target_dim
+        features = features.reshape(len(features), 1, -1)
+        features = F.adaptive_avg_pool1d(features, self.target_dim)
+        return features.reshape(len(features), -1)
+
+
+class NetworkFeatureAggregator(torch.nn.Module):
+    """Efficient extraction of network features."""
+
+    def __init__(self, backbone, layers_to_extract_from, device):
+        super(NetworkFeatureAggregator, self).__init__()
+        """Extraction of network features.
+
+        Runs a network only to the last layer of the list of layers where
+        network features should be extracted from.
+
+        Args:
+            backbone: torchvision.model
+            layers_to_extract_from: [list of str]
+        """
+        self.layers_to_extract_from = layers_to_extract_from
+        self.backbone = backbone
+        self.device = device
+        if not hasattr(backbone, "hook_handles"):
+            self.backbone.hook_handles = []
+        for handle in self.backbone.hook_handles:
+            handle.remove()
+        self.outputs = {}
+
+        for extract_layer in layers_to_extract_from:
+            forward_hook = ForwardHook(
+                self.outputs, extract_layer, layers_to_extract_from[-1]
+            )
+            if "." in extract_layer:
+                extract_block, extract_idx = extract_layer.split(".")
+                network_layer = backbone.__dict__["_modules"][extract_block]
+                if extract_idx.isnumeric():
+                    extract_idx = int(extract_idx)
+                    network_layer = network_layer[extract_idx]
+                else:
+                    network_layer = network_layer.__dict__["_modules"][extract_idx]
+            else:
+                network_layer = backbone.__dict__["_modules"][extract_layer]
+
+            if isinstance(network_layer, torch.nn.Sequential):
+                self.backbone.hook_handles.append(
+                    network_layer[-1].register_forward_hook(forward_hook)
+                )
+            else:
+                self.backbone.hook_handles.append(
+                    network_layer.register_forward_hook(forward_hook)
+                )
+        self.to(self.device)
+
+    def forward(self, images):
+        self.outputs.clear()
+        with torch.no_grad():
+            # The backbone will throw an Exception once it reached the last
+            # layer to compute features from. Computation will stop there.
+            try:
+                _ = self.backbone(images)
+            except LastLayerToExtractReachedException:
+                pass
+        return self.outputs
+
+    def feature_dimensions(self, input_shape):
+        """Computes the feature dimensions for all layers given input_shape."""
+        _input = torch.ones([1] + list(input_shape)).to(self.device)
+        _output = self(_input)
+        return [_output[layer].shape[1] for layer in self.layers_to_extract_from]
+
+
+class ForwardHook:
+    def __init__(self, hook_dict, layer_name: str, last_layer_to_extract: str):
+        self.hook_dict = hook_dict
+        self.layer_name = layer_name
+        self.raise_exception_to_break = copy.deepcopy(
+            layer_name == last_layer_to_extract
+        )
+
+    def __call__(self, module, input, output):
+        self.hook_dict[self.layer_name] = output
+        if self.raise_exception_to_break:
+            raise LastLayerToExtractReachedException()
+        return
+
+
+class LastLayerToExtractReachedException(Exception):
+    pass
+
+
+# Image handling classes.
+class PatchMaker:
+    def __init__(self, patchsize, stride=None):
+        self.patchsize = patchsize
+        self.stride = stride
+
+    def patchify(self, features, return_spatial_info=False):
+        """Convert a tensor into a tensor of respective patches.
+        Args:
+            x: [torch.Tensor, bs x c x w x h]
+        Returns:
+            x: [torch.Tensor, bs * w//stride * h//stride, c, patchsize,
+            patchsize]
+        """
+        padding = int((self.patchsize - 1) / 2)
+        unfolder = torch.nn.Unfold(
+            kernel_size=self.patchsize, stride=self.stride, padding=padding, dilation=1
+        )
+        unfolded_features = unfolder(features)
+        number_of_total_patches = []
+        for s in features.shape[-2:]:
+            n_patches = (
+                s + 2 * padding - 1 * (self.patchsize - 1) - 1
+            ) / self.stride + 1
+            number_of_total_patches.append(int(n_patches))
+        unfolded_features = unfolded_features.reshape(
+            *features.shape[:2], self.patchsize, self.patchsize, -1
+        )
+        unfolded_features = unfolded_features.permute(0, 4, 1, 2, 3)
+
+        if return_spatial_info:
+            return unfolded_features, number_of_total_patches
+        return unfolded_features
+
+    def unpatch_scores(self, x, batchsize):
+        return x.reshape(batchsize, -1, *x.shape[1:])
+
+    def score(self, x):
+        was_numpy = False
+        if isinstance(x, np.ndarray):
+            was_numpy = True
+            x = torch.from_numpy(x)
+        while x.ndim > 1:
+            x = torch.max(x, dim=-1).values
+        if was_numpy:
+            return x.numpy()
+        return x
+
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+'''
+
+dataset_root = "G:/Bameri_Thesis/patchcore/patchcore-inspection-main/mvtec_original"
+features_save_root = "G:/Bameri_Thesis/patchcore/patchcore-inspection-main/features_train"
+os.makedirs(features_save_root, exist_ok=True)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = PatchCore(device)
+
+# 2. بارگذاری backbone و تنظیمات
+backbone = wide_resnet50_2(weights='IMAGENET1K_V1')
+layers_to_extract_from = ["layer1", "layer2"]
+input_shape = (3, 224, 224)
+
+model.load(
+    backbone=backbone,
+    layers_to_extract_from=layers_to_extract_from,
+    device=device,
+    input_shape=input_shape,
+    pretrain_embed_dimension=1024,
+    target_embed_dimension=384,
+)
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
+
+# پردازش کلاس‌ها
+for class_name in os.listdir(dataset_root):
+    class_train_dir = os.path.join(dataset_root, class_name, "train")
+    if not os.path.isdir(class_train_dir):
+        continue
+
+    class_features_dir = os.path.join(features_save_root, class_name)
+    os.makedirs(class_features_dir, exist_ok=True)
+
+    train_dir = os.path.join(class_train_dir, "good")
+    if not os.path.isdir(train_dir):
+        continue
+
+    image_files = [f for f in os.listdir(train_dir) if f.lower().endswith((".png", ".jpg", ".jpeg"))]
+    print(f"Found {len(image_files)} images in class {class_name}")
+
+    for img_name in tqdm(image_files, desc=f"Processing {class_name}"):
+        img_path = os.path.join(train_dir, img_name)
+        img = Image.open(img_path).convert("RGB")
+        img_tensor = transform(img)
+
+        feature_vector = model.embed(img_tensor.unsqueeze(0).to(device))
+
+        feature_file = os.path.join(class_features_dir, os.path.splitext(img_name)[0] + ".npy")
+        np.save(feature_file, feature_vector)
